7964ca87d5b55d81b306c660a2aede6f1aa1661c
Kofrey <kofrey@vk.com>
2025-12-18 16:36:58 +0300

    update

---
+++ Assets/Scripts/AudioToggler.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioToggler : MonoBehaviour
+{
+    [SerializeField] private Toggle _toggle;
+    [SerializeField] private AudioListener _listener; 
+
+    private string _voiceEnabledName = "VoiceEnabled";
+
+    private void Start() 
+    {
+        _toggle.onValueChanged.AddListener(ToggleAudio);
+        _toggle.isOn = PlayerPrefs.GetInt(_voiceEnabledName, 1) == 1;
+    }
+
+    public void ToggleAudio(bool enabled)
+    {
+        if (enabled)
+            _listener.enabled = true;
+        else 
+            _listener.enabled = false;
+
+        PlayerPrefs.SetInt(_voiceEnabledName, enabled ? 1 : 0);
+    }
+}
